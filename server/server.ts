66acf95cf973e879651839249d26c49abe6d58c3
Trey Dedman <treydedman@gmail.com>
2025-04-11 19:10:50 -0500

    Project init

---
+++ server/server.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import pg from 'pg';
+import express from 'express';
+import { ClientError, errorMiddleware } from './lib/index.js';
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/test', async (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.use(errorMiddleware);
+
+app.listen(process.env.PORT, () => {
+  console.log(`express server listening on port ${process.env.PORT}`);
+});
